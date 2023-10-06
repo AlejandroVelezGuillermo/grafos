9988ee95d39811287394da4037b6a2eddd0c597d
AlejandroVelezGuillermo <125221168+AlejandroVelezGuillermo@users.noreply.github.com>
2023-10-05 18:53:14 -0600

    Add files via upload

---
+++ grafos/grafos/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace grafos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddNode = new System.Windows.Forms.Button();
+            this.btnAddEdge = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAddNode
+            // 
+            this.btnAddNode.Location = new System.Drawing.Point(30, 29);
+            this.btnAddNode.Name = "btnAddNode";
+            this.btnAddNode.Size = new System.Drawing.Size(75, 23);
+            this.btnAddNode.TabIndex = 0;
+            this.btnAddNode.Text = "button1";
+            this.btnAddNode.UseVisualStyleBackColor = true;
+            this.btnAddNode.Click += new System.EventHandler(this.btnAddNode_Click);
+            // 
+            // btnAddEdge
+            // 
+            this.btnAddEdge.Location = new System.Drawing.Point(129, 29);
+            this.btnAddEdge.Name = "btnAddEdge";
+            this.btnAddEdge.Size = new System.Drawing.Size(75, 23);
+            this.btnAddEdge.TabIndex = 1;
+            this.btnAddEdge.Text = "button2";
+            this.btnAddEdge.UseVisualStyleBackColor = true;
+            this.btnAddEdge.Click += new System.EventHandler(this.btnAddEdge_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(244, 28);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnAddEdge);
+            this.Controls.Add(this.btnAddNode);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddNode;
+        private System.Windows.Forms.Button btnAddEdge;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ grafos/grafos/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace grafos
+{
+    private List<Point> nodes = new List<Point>();
+    private List<Tuple<int, int>> edges = new List<Tuple<int, int>>();
+    private bool isAddingNode = false;
+    private int selectedNode = -1;
+    public partial class Form1 : Form
+    {
+        private bool isAddingNode;
+        private int selectedNode;
+        private object nodes;
+        private object panel1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawGraph(e.Graphics);
+        }
+        private void DrawGraph(Graphics g)
+        {
+            // Dibujar bordes
+            foreach (var edge in edges)
+            {
+                Point node1 = nodes[edge.Item1];
+                Point node2 = nodes[edge.Item2];
+                g.DrawLine(Pens.Black, node1, node2);
+            }
+
+            // Dibujar nodos
+            foreach (var node in nodes)
+            {
+                g.FillEllipse(Brushes.Blue, node.X - 10, node.Y - 10, 20, 20);
+            }
+        }
+
+        private object GetPanel1()
+        {
+            return panel1;
+        }
+
+        private void btnAddNode_Click(object sender, EventArgs e, object panel1)
+        {
+            isAddingNode = true;
+            selectedNode = -1;
+            panel1.Cursor = Cursors.Cross;
+        }
+
+        private void btnAddEdge_Click(object sender, EventArgs e)
+        {
+            isAddingNode = false;
+            selectedNode = -1;
+            panel1.Cursor = Cursors.Default;
+        }
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (isAddingNode)
+            {
+                nodes.Add(e.Location);
+                panel1.Invalidate();
+            }
+            else
+            {
+                // Agregar bordes haciendo clic en dos nodos
+                if (selectedNode == -1)
+                {
+                    foreach (var node in nodes)
+                    {
+                        if (Distance(e.Location, node) < 10)
+                        {
+                            selectedNode = nodes.IndexOf(node);
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (var node in nodes)
+                    {
+                        if (Distance(e.Location, node) < 10)
+                        {
+                            int secondNode = nodes.IndexOf(node);
+                            if (selectedNode != secondNode)
+                            {
+                                edges.Add(new Tuple<int, int>(selectedNode, secondNode));
+                                panel1.Invalidate();
+                            }
+                            break;
+                        }
+                    }
+                    selectedNode = -1;
+                }
+            }
+        }
+        private double Distance(Point p1, Point p2)
+        {
+            int dx = p2.X - p1.X;
+            int dy = p2.Y - p1.Y;
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+    }
+}
